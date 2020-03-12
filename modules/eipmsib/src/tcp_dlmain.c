134f33f0e4c2f6ae775bd0dbd44d5a416ba78d62
ythan <bang9211@uangel.com>
2020-03-12 12:30:47 +0900

    [eipmsib]TCP Server 추가

--- modules/eipmsib/src/tcp_dlmain.c
+++ modules/eipmsib/src/tcp_dlmain.c
@@ -3,9 +3,9 @@
 #include <upa/upa_tcp.h>
 
 /**
- * @brief DBIF 메시지를 받아 TCP Server 에 전송한다.
+ * @brief DBIF 메시지를 받아 eIPMS 의 TCP Server 에 전송한다.
  */
-int tcp_handle_clireq(uxc_action_t *action, uxc_worker_t* worker, uxc_msg_t *msg)
+int tcp_handle_gwreq(uxc_action_t *action, uxc_worker_t* worker, uxc_msg_t *msg)
 {
 	uxc_ipcmsg_t *ipcmsg;
 	tcp_client_t *client;
@@ -18,9 +18,9 @@ int tcp_handle_clireq(uxc_action_t *action, uxc_worker_t* worker, uxc_msg_t *msg
 }
 
 /**
- * @brief TCP Server 로부터 메시지를 받아 DBIF 에 전송한다. 
+ * @brief eIPMS 의 TCP Server 로부터 메시지를 받아 DBIF 에 전송한다. 
  */
-int tcp_handle_clirsp(uxc_action_t *action, uxc_worker_t* worker, uxc_msg_t *uxcmsg)
+int tcp_handle_eipmsrsp(uxc_action_t *action, uxc_worker_t* worker, uxc_msg_t *uxcmsg)
 {
 	int rv;
 	tcp_client_t *client;
@@ -41,13 +41,13 @@ int tcp_handle_clirsp(uxc_action_t *action, uxc_worker_t* worker, uxc_msg_t *uxc
 }
 
 /**
- * @brief TCP Client로 부ㅌ터 TCP DBIF msg 를 받아 decode/encode 후 ipcmsg 를 uxcutor 로 전송한다. 
+ * @brief eIPMS 의 TCP Client 로부터 TCP DBIF msg 를 받아 decode/encode 후 ipcmsg 를 uxcutor 로 전송한다. 
  * @param action action
  * @param worker worker
  * @param msg uxcutor message
  * @return 실행 결과
  */
-int tcp_handle_svrreq( uxc_action_t *action, uxc_worker_t* worker, uxc_msg_t *uxcmsg)
+int tcp_handle_testsvrreq( uxc_action_t *action, uxc_worker_t* worker, uxc_msg_t *uxcmsg)
 {
 	upa_tcpmsg_t *tcpmsg;
 	tcpmsg = (upa_tcpmsg_t *) uxcmsg->data;
@@ -65,3 +65,27 @@ int tcp_handle_svrreq( uxc_action_t *action, uxc_worker_t* worker, uxc_msg_t *ux
 	return 0;
 }
 
+/**
+ * @brief Test TCP Server 에서 IB TCP Client 로부터 TCP DBIF msg 를 받아 decode/encode 후 ipcmsg 를 uxcutor 로 전송한다. 
+ * @param action action
+ * @param worker worker
+ * @param msg uxcutor message
+ * @return 실행 결과
+ */
+int tcp_handle_test_svrreq( uxc_action_t *action, uxc_worker_t* worker, uxc_msg_t *uxcmsg)
+{
+	upa_tcpmsg_t *tcpmsg;
+	tcpmsg = (upa_tcpmsg_t *) uxcmsg->data;
+
+	if ( tcpmsg->evtype == UPA_TCP_EVENT_RECV) {
+		ux_log( UXL_INFO, "Receive event! (len=%d, worker=%d, chnl_idx=%d, peer_key=%d)",
+				tcpmsg->netmsg->buflen, uxc_worker_get_thread_id(worker),
+				tcpmsg->peerkey.chnl_idx, tcpmsg->peerkey.peer_key);
+		return tcp_server_handle_svrreq( tcp_server_instance(), worker, tcpmsg);
+	} else {
+		ux_log( UXL_INFO, "invalid event");
+		return -1;
+	}
+
+	return 0;
+}
