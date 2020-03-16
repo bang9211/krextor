1abcfc9ae044790880201c046068c942dc8a498d
ythan <bang9211@uangel.com>
2020-03-16 13:50:26 +0900

    Revert "Revert "[eipmsib] tcp 메시지에서  sessionID, gwSessionID 생략""
    This reverts commit 09b25ee907be18aab6214dc3fa6f3e93e6b7a92b.

--- modules/testtcpserver/src/tcp_server.c
+++ modules/testtcpserver/src/tcp_server.c
@@ -7,9 +7,7 @@
 static tcp_server_t *_g_server = NULL;
 
 static void _tcp_server_destroy( uxc_plugin_t *pi);
-static int tcp_server_handle_idpreq( tcp_server_t *server, uxc_worker_t *worker,
-					upa_tcpmsg_t *tcpmsg, tcp_msg_t *msg );
-static int tcp_server_handle_clicktocall_req( tcp_server_t *server, uxc_worker_t *worker,
+static int tcp_server_handle_clicktocall_start_req( tcp_server_t *server, uxc_worker_t *worker,
 					upa_tcpmsg_t *tcpmsg, tcp_msg_t *msg );
 static int _tcp_server_get_thrid( uxc_paif_t *paif, uxc_msg_t *msg);
 static int _tcp_server_on_accept(upa_tcp_t *tcp, ux_channel_t *channel, ux_accptor_t *accptor,
@@ -100,56 +98,58 @@ int tcp_server_handle_svrreq( tcp_server_t *server, uxc_worker_t* worker, upa_tc
 	switch(msgID)
 	{
 		case TCP_MSGID_CLICKTOCALL_START:
-			return tcp_server_handle_clicktocall_req(server, worker, tcpmsg, msg);
+			return tcp_server_handle_clicktocall_start_req(server, worker, tcpmsg, msg);
 		default:
 			break;	
 	}			
 	return -1;
 }
 
-static int tcp_server_handle_clicktocall_req( tcp_server_t *server, uxc_worker_t *worker,
+static int tcp_server_handle_clicktocall_start_req( tcp_server_t *server, uxc_worker_t *worker,
 					upa_tcpmsg_t *tcpmsg, tcp_msg_t *msg )
 {
 	int rv;
-	tcp_clicktocall_start_rsp_t clicktocall_rsp[1];
-	tcp_clicktocall_start_req_t clicktocall_req[1];
+	tcp_clicktocall_start_rsp_t clicktocall_start_rsp[1];
+	tcp_clicktocall_start_req_t clicktocall_start_req[1];
 
-	ux_log(UXL_INFO, "* tcp_server_handle_clicktocall_req ");
-	tcp_clicktocall_start_req_init(clicktocall_req);
-	tcp_clicktocall_start_rsp_init(clicktocall_rsp);
+	ux_log(UXL_INFO, "* tcp_server_handle_clicktocall_start_req ");
+	tcp_clicktocall_start_req_init(clicktocall_start_req);
+	tcp_clicktocall_start_rsp_init(clicktocall_start_rsp);
 
-	rv = tcp_clicktocall_start_req_decode_msg(clicktocall_req, msg);
+	rv = tcp_clicktocall_start_req_decode_msg(clicktocall_start_req, msg);
 	if (rv <eUXC_SUCCESS) return rv;
 
 	/* To do.. */	
-	ux_log(UXL_CRT, "  [sessionID] %s", clicktocall_req->sessionID);
-	ux_log(UXL_CRT, "  [gwSessionID] %s", clicktocall_req->gwSessionID);
-	ux_log(UXL_CRT, "  [subscriberName] %s", clicktocall_req->subscriberName);
-	ux_log(UXL_CRT, "  [recordingType] %d", clicktocall_req->recordingType);
-	ux_log(UXL_CRT, "  [callingNumber] %s", clicktocall_req->callingNumber);
-	ux_log(UXL_CRT, "  [calledNumber] %s", clicktocall_req->calledNumber);
-	ux_log(UXL_CRT, "  [serviceCode] %d", clicktocall_req->serviceCode);
-	ux_log(UXL_CRT, "  [ringBackToneType] %d", clicktocall_req->ringBackToneType);
-	ux_log(UXL_CRT, "  [waitingMentID] %d", clicktocall_req->waitingMentID);
-	ux_log(UXL_CRT, "  [scenarioType] %d", clicktocall_req->scenarioType);
-	ux_log(UXL_CRT, "  [callMentID] %d", clicktocall_req->callMentID);
-	ux_log(UXL_CRT, "  [callingCID] %s", clicktocall_req->callingCID);
-	ux_log(UXL_CRT, "  [calledCID] %s", clicktocall_req->calledCID);
-	ux_log(UXL_CRT, "  [recordingFileName] %s", clicktocall_req->recordingFileName);
-	ux_log(UXL_CRT, "  [isAllRecording] %d", clicktocall_req->isAllRecording);
-	ux_log(UXL_CRT, "  [endIfRecordingFailed] %d", clicktocall_req->endIfRecordingFailed);
-	ux_log(UXL_CRT, "  [endIfRecordingEnded] %d", clicktocall_req->endIfRecordingEnded);
-	ux_log(UXL_CRT, "  [hostingCode] %d", clicktocall_req->hostingCode);
-	ux_log(UXL_CRT, "  [wirelessTimeout] %d", clicktocall_req->wirelessTimeout);
-	ux_log(UXL_CRT, "  [wiredTimeout] %d", clicktocall_req->wiredTimeout);
+	ux_log(UXL_CRT, "  [sessionID] %s", clicktocall_start_req->sessionID);
+	ux_log(UXL_CRT, "  [gwSessionID] %s", clicktocall_start_req->gwSessionID);
+	ux_log(UXL_CRT, "  [subscriberName] %s", clicktocall_start_req->subscriberName);
+	ux_log(UXL_CRT, "  [recordingType] %d", clicktocall_start_req->recordingType);
+	ux_log(UXL_CRT, "  [callingNumber] %s", clicktocall_start_req->callingNumber);
+	ux_log(UXL_CRT, "  [calledNumber] %s", clicktocall_start_req->calledNumber);
+	ux_log(UXL_CRT, "  [serviceCode] %d", clicktocall_start_req->serviceCode);
+	ux_log(UXL_CRT, "  [ringBackToneType] %d", clicktocall_start_req->ringBackToneType);
+	ux_log(UXL_CRT, "  [waitingMentID] %d", clicktocall_start_req->waitingMentID);
+	ux_log(UXL_CRT, "  [scenarioType] %d", clicktocall_start_req->scenarioType);
+	ux_log(UXL_CRT, "  [callMentID] %d", clicktocall_start_req->callMentID);
+	ux_log(UXL_CRT, "  [callingCID] %s", clicktocall_start_req->callingCID);
+	ux_log(UXL_CRT, "  [calledCID] %s", clicktocall_start_req->calledCID);
+	ux_log(UXL_CRT, "  [recordingFileName] %s", clicktocall_start_req->recordingFileName);
+	ux_log(UXL_CRT, "  [isAllRecording] %d", clicktocall_start_req->isAllRecording);
+	ux_log(UXL_CRT, "  [endIfRecordingFailed] %d", clicktocall_start_req->endIfRecordingFailed);
+	ux_log(UXL_CRT, "  [endIfRecordingEnded] %d", clicktocall_start_req->endIfRecordingEnded);
+	ux_log(UXL_CRT, "  [hostingCode] %d", clicktocall_start_req->hostingCode);
+	ux_log(UXL_CRT, "  [wirelessTimeout] %d", clicktocall_start_req->wirelessTimeout);
+	ux_log(UXL_CRT, "  [wiredTimeout] %d", clicktocall_start_req->wiredTimeout);
 
-	clicktocall_rsp->result = 0;
-	clicktocall_rsp->callTime = 0;
-	clicktocall_rsp->finalUnitAction = 100;
-	clicktocall_rsp->brandId = 0;
-	clicktocall_rsp->lang= 0;
+	clicktocall_start_rsp->resultCode = 0;
+	clicktocall_start_rsp->serviceID = "service0001";
+	clicktocall_start_rsp->isRecording = 0;
+	clicktocall_start_rsp->filler1 = 0;
+	clicktocall_start_rsp->filler2= 0;
+	clicktocall_start_rsp->recordingFileURL= "/test/test";
+	clicktocall_start_rsp->recordingFileName= "testFileName";
 
-	rv = tcp_clicktocall_start_rsp_encode_ipcmsg(clicktocall_rsp, msg);
+	rv = tcp_clicktocall_start_rsp_encode_ipcmsg(clicktocall_start_rsp, msg);
 	if (rv <eUXC_SUCCESS) return rv;
 	rv = tcp_msg_send(msg, server->patcp, &tcpmsg->peerkey);	
 	if (rv <eUXC_SUCCESS) return rv;
